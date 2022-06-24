9a15bd8aacf59ae2827f8451bfdcbd9c30e095cd
JimCoder-Dev <jamesholmson88@gmail.com>
2022-06-24 18:35:10 +0100

    Created error bounderies

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,7 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
   // constructor(props) {
@@ -22,10 +24,12 @@ class Details extends Component {
       return <h2>Loading...</h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
 
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>
@@ -41,6 +45,10 @@ class Details extends Component {
 
 const WrappedDetails = () => {
   const params = useParams();
-  return <Details params={params} />;
+  return (
+    <ErrorBoundary>
+      <Details params={params} />
+    </ErrorBoundary>
+  );
 };
 export default WrappedDetails;
